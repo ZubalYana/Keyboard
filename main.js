064655bd453e2cfb339e0e09601988fff8f2765e
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2023-12-19 22:18:21 +0200

    add all files

---
+++ main.js
@@ -0,0 +1,399 @@
+document.body.onkeydown = function(e){
+    console.log(e);
+    console.log(e.keyCode);
+    if(e.keyCode==27){
+    Esc.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==112){
+        MyF1.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==113){
+        MyF2.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==114){
+        MyF3.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==115){
+        MyF4.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==116){
+        MyF5.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==117){
+        MyF6.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==118){
+        MyF7.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==119){
+        MyF8.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==120){
+        MyF9.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==121){
+        MyF10.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==122){
+        MyF11.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==123){
+        MyF12.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==46){
+        Del.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==33){
+        PgUp.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==36){
+        Home.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==34){
+        PgDn.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==35){
+        End.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==192){
+        EEeeeeee.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==49){
+        Number1.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==50){
+        Number2.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==51){
+        Number3.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==52){
+        Number4.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==53){
+        Number5.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==54){
+        Number6.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==55){
+        Number7.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==56){
+        Number8.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==57){
+        Number9.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==48){
+        Number0.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==189){
+        Number11.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==187){
+        Number12.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==8){
+        Backspace.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==225){
+        Nitro.style.transform = 'scale(0.9)'; // ця кнопка втілення зла
+    }else if(e.keyCode==144){
+        NumLk.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==111){
+        palka.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==106){
+        star.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==9){
+        Tab.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==81){
+        Qqq.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==87){
+        Www.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==69){
+        Eee.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==82){
+        Rrr.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==84){
+        Ttt.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==89){
+        Yyy.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==85){
+        Uuu.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==73){
+        Iii.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==79){
+        Ooo.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==80){
+        Ppp.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==219){
+        Xxxx.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==221){
+        Yyyy.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==220){
+        Rrrr.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==103){
+        seven.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==104){
+        eight.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==105){
+        nine.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==109){
+        minus.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==20){
+        CapsLosk.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==65){
+        Aaa.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==83){
+        Sss.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==68){
+        Ddd.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==70){
+        Fff.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==71){
+        Ggg.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==72){
+        Hhh.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==74){
+        Jjj.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==75){
+        Kkk.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==76){
+        Lll.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==186){
+        Jjjj.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==222){
+        Eeee.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==13){
+        Enter.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==37){
+        four.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==12){
+        five.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==39){
+        six.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==107){
+        plus.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==16){
+        Shift.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==90){
+        Zzz.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==88){
+        Xxx.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==67){
+        Ccc.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==86){
+        Vvv.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==66){
+        Bbb.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==78){
+        Nnn.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==77){
+        Mmm.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==188){
+        Bbbbb.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==190){
+        Uuuuu.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==191){
+        Baaaaaaar.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==38){
+        CaretUp.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==37){
+        CaretLeft.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==40){
+        CaretDown.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==39){
+        CaretRight.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==45){
+        Zero.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==97){
+        one.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==98){
+        two.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==99){
+        three.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==13){
+        Enter2.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==17){
+        Ctrl.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==91){
+        Windows.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==18){
+        AltLeft.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==32){
+        Space.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==93){
+        List.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==96){
+        Zero.style.transform = 'scale(0.9)';
+    }else if(e.keyCode==110){
+        Dellll.style.transform = 'scale(0.9)';
+    }
+}
+
+document.body.onkeyup = function(e){
+    if(e.keyCode==27){
+        Esc.style.transform = 'scale(1)';
+     }else if(e.keyCode==112){
+        MyF1.style.transform = 'scale(1)';
+    }else if(e.keyCode==113){
+        MyF2.style.transform = 'scale(1)';
+    }else if(e.keyCode==114){
+        MyF3.style.transform = 'scale(1)';
+    }else if(e.keyCode==115){
+        MyF4.style.transform = 'scale(1)';
+    }else if(e.keyCode==116){
+        MyF5.style.transform = 'scale(1)';
+    }else if(e.keyCode==117){
+        MyF6.style.transform = 'scale(1)';
+    }else if(e.keyCode==118){
+        MyF7.style.transform = 'scale(1)';
+    }else if(e.keyCode==119){
+        MyF8.style.transform = 'scale(1)';
+    }else if(e.keyCode==120){
+        MyF9.style.transform = 'scale(1)';
+    }else if(e.keyCode==121){
+        MyF10.style.transform = 'scale(1)';
+    }else if(e.keyCode==122){
+        MyF11.style.transform = 'scale(1)';
+    }else if(e.keyCode==123){
+        MyF12.style.transform = 'scale(1)';
+    }else if(e.keyCode==46){
+        Del.style.transform = 'scale(1)';
+    }else if(e.keyCode==33){
+        PgUp.style.transform = 'scale(1)';
+    }else if(e.keyCode==36){
+        Home.style.transform = 'scale(1)';
+    }else if(e.keyCode==34){
+        PgDn.style.transform = 'scale(1)';
+    }else if(e.keyCode==35){
+        End.style.transform = 'scale(1)';
+    }else if(e.keyCode==192){
+        EEeeeeee.style.transform = 'scale(1)';
+    }else if(e.keyCode==49){
+        Number1.style.transform = 'scale(1)';
+    }else if(e.keyCode==50){
+        Number2.style.transform = 'scale(1)';
+    }else if(e.keyCode==51){
+        Number3.style.transform = 'scale(1)';
+    }else if(e.keyCode==52){
+        Number4.style.transform = 'scale(1)';
+    }else if(e.keyCode==53){
+        Number5.style.transform = 'scale(1)';
+    }else if(e.keyCode==54){
+        Number6.style.transform = 'scale(1)';
+    }else if(e.keyCode==55){
+        Number7.style.transform = 'scale(1)';
+    }else if(e.keyCode==56){
+        Number8.style.transform = 'scale(1)';
+    }else if(e.keyCode==57){
+        Number9.style.transform = 'scale(1)';
+    }else if(e.keyCode==48){
+        Number0.style.transform = 'scale(1)';
+    }else if(e.keyCode==189){
+        Number11.style.transform = 'scale(1)';
+    }else if(e.keyCode==187){
+        Number12.style.transform = 'scale(1)';
+    }else if(e.keyCode==8){
+        Backspace.style.transform = 'scale(1)';
+    }else if(e.keyCode==225){
+        Nitro.style.transform = 'scale(1)';
+    }else if(e.keyCode==144){
+        NumLk.style.transform = 'scale(1)';
+    }else if(e.keyCode==111){
+        palka.style.transform = 'scale(1)';
+    }else if(e.keyCode==106){
+        star.style.transform = 'scale(1)';
+    }else if(e.keyCode==9){
+        Tab.style.transform = 'scale(1)';
+    }else if(e.keyCode==81){
+        Qqq.style.transform = 'scale(1)';
+    }else if(e.keyCode==87){
+        Www.style.transform = 'scale(1)';
+    }else if(e.keyCode==69){
+        Eee.style.transform = 'scale(1)';
+    }else if(e.keyCode==82){
+        Rrr.style.transform = 'scale(1)';
+    }else if(e.keyCode==84){
+        Ttt.style.transform = 'scale(1)';
+    }else if(e.keyCode==89){
+        Yyy.style.transform = 'scale(1)';
+    }else if(e.keyCode==85){
+        Uuu.style.transform = 'scale(1)';
+    }else if(e.keyCode==73){
+        Iii.style.transform = 'scale(1)';
+    }else if(e.keyCode==79){
+        Ooo.style.transform = 'scale(1)';
+    }else if(e.keyCode==80){
+        Ppp.style.transform = 'scale(1)';
+    }else if(e.keyCode==219){
+        Xxxx.style.transform = 'scale(1)';
+    }else if(e.keyCode==221){
+        Yyyy.style.transform = 'scale(1)';
+    }else if(e.keyCode==220){
+        Rrrr.style.transform = 'scale(1)';
+    }else if(e.keyCode==103){
+        seven.style.transform = 'scale(1)';
+    }else if(e.keyCode==104){
+        eight.style.transform = 'scale(1)';
+    }else if(e.keyCode==105){
+        nine.style.transform = 'scale(1)';
+    }else if(e.keyCode==109){
+        minus.style.transform = 'scale(1)';
+    }else if(e.keyCode==20){
+        CapsLock.style.transform = 'scale(1)';
+    }else if(e.keyCode==65){
+        Aaa.style.transform = 'scale(1)';
+    }else if(e.keyCode==83){
+        Sss.style.transform = 'scale(1)';
+    }else if(e.keyCode==68){
+        Ddd.style.transform = 'scale(1)';
+    }else if(e.keyCode==70){
+        Fff.style.transform = 'scale(1)';
+    }else if(e.keyCode==71){
+        Ggg.style.transform = 'scale(1)';
+    }else if(e.keyCode==72){
+        Hhh.style.transform = 'scale(1)';
+    }else if(e.keyCode==74){
+        Jjj.style.transform = 'scale(1)';
+    }else if(e.keyCode==75){
+        Kkk.style.transform = 'scale(1)';
+    }else if(e.keyCode==76){
+        Lll.style.transform = 'scale(1)';
+    }else if(e.keyCode==186){
+        Jjjj.style.transform = 'scale(1)';
+    }else if(e.keyCode==222){
+        Eeee.style.transform = 'scale(1)';
+    }else if(e.keyCode==13){
+        Enter.style.transform = 'scale(1)';
+    }else if(e.keyCode==37){
+        four.style.transform = 'scale(1)';
+    }else if(e.keyCode==12){
+        five.style.transform = 'scale(1)';
+    }else if(e.keyCode==39){
+        six.style.transform = 'scale(1)';
+    }else if(e.keyCode==107){
+        plus.style.transform = 'scale(1)';
+    }else if(e.keyCode==16){
+        Shift.style.transform = 'scale(1)';
+    }else if(e.keyCode==90){
+        Zzz.style.transform = 'scale(1)';
+    }else if(e.keyCode==88){
+        Xxx.style.transform = 'scale(1)';
+    }else if(e.keyCode==67){
+        Ccc.style.transform = 'scale(1)';
+    }else if(e.keyCode==86){
+        Vvv.style.transform = 'scale(1)';
+    }else if(e.keyCode==66){
+        Bbb.style.transform = 'scale(1)';
+    }else if(e.keyCode==78){
+        Nnn.style.transform = 'scale(1)';
+    }else if(e.keyCode==77){
+        Mmm.style.transform = 'scale(1)';
+    }else if(e.keyCode==188){
+        Bbbbb.style.transform = 'scale(1)';
+    }else if(e.keyCode==190){
+        Uuuuu.style.transform = 'scale(1)';
+    }else if(e.keyCode==191){
+        Baaaaaaar.style.transform = 'scale(1)';
+    }else if(e.keyCode==38){
+        CaretUp.style.transform = 'scale(1)';
+    }else if(e.keyCode==37){
+        CaretLeft.style.transform = 'scale(1)';
+    }else if(e.keyCode==40){
+        CaretDown.style.transform = 'scale(1)';
+    }else if(e.keyCode==39){
+        CaretRight.style.transform = 'scale(1)';
+    }else if(e.keyCode==45){
+        Zero.style.transform = 'scale(1)';
+    }else if(e.keyCode==97){
+        one.style.transform = 'scale(1)';
+    }else if(e.keyCode==98){
+        two.style.transform = 'scale(1)';
+    }else if(e.keyCode==99){
+        three.style.transform = 'scale(1)';
+    }else if(e.keyCode==13){
+        Enter2.style.transform = 'scale(1)';
+    }else if(e.keyCode==17){
+        Ctrl.style.transform = 'scale(1)';
+    }else if(e.keyCode==91){
+        Windows.style.transform = 'scale(1)';
+    }else if(e.keyCode==32){
+        Space.style.transform = 'scale(1)';
+    }else if(e.keyCode==93){
+        List.style.transform = 'scale(1)';
+    }else if(e.keyCode==96){
+        Zero.style.transform = 'scale(1)';
+    }else if(e.keyCode==110){
+        Dellll.style.transform = 'scale(1)';
+    }
+}
